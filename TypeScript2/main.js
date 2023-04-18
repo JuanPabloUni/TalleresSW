15d7ad8abb145fe12268525a8b4e2131e49b52e5
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-04-18 00:20:56 -0500

    TS2

--- TypeScript2/main.js
+++ TypeScript2/main.js
@@ -1,20 +1,48 @@
 import { lista_series } from './Data.js';
 var tableBody = document.querySelector('#series-table tbody');
 var tableFooter = document.querySelector('#series-table tfoot');
+var seriesDetail = document.querySelector('#series-detail');
 var totalSeries = lista_series.length;
 var totalSeasons = 0;
+function createCard(series) {
+    var card = document.createElement('div');
+    card.classList.add('card', 'my-3');
+    var cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    var cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = series.getName();
+    cardBody.appendChild(cardTitle);
+    var cardSubtitle = document.createElement('h6');
+    cardSubtitle.classList.add('card-subtitle', 'mb-2', 'text-muted');
+    cardSubtitle.textContent = "Channel: ".concat(series.getChannel(), " | Seasons: ").concat(series.getSeasons());
+    cardBody.appendChild(cardSubtitle);
+    var cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    // Replace with actual description if available
+    cardText.textContent = 'No description available.';
+    cardBody.appendChild(cardText);
+    card.appendChild(cardBody);
+    return card;
+}
 if (tableBody !== null) {
-    for (var _i = 0, lista_series_1 = lista_series; _i < lista_series_1.length; _i++) {
-        var Series_1 = lista_series_1[_i];
+    var _loop_1 = function (Series_1) {
         var row = document.createElement('tr');
-        row.innerHTML = "\n          <td>".concat(Series_1.getId(), "</td>\n          <td>").concat(Series_1.getName(), "</td>\n          <td>").concat(Series_1.getChannel(), "</td>\n          <td>").concat(Series_1.getSeasons(), "</td>\n        ");
+        row.innerHTML = "\n      <td>".concat(Series_1.getId(), "</td>\n      <td>").concat(Series_1.getName(), "</td>\n      <td>").concat(Series_1.getChannel(), "</td>\n      <td>").concat(Series_1.getSeasons(), "</td>\n    ");
         tableBody.appendChild(row);
         totalSeasons += Series_1.getSeasons();
-    }
-    if (tableFooter !== null) {
-        var avgSeasons = totalSeasons / totalSeries;
-        var footerRow = document.createElement('tr');
-        footerRow.innerHTML = "\n        <td colspan=\"3\" style=\"text-align: right;\"><strong>Average Seasons:</strong></td>\n        <td>".concat(avgSeasons.toFixed(2), "</td>\n        ");
-        tableFooter.appendChild(footerRow);
+        row.addEventListener('click', function () {
+            if (seriesDetail !== null) {
+                while (seriesDetail.firstChild) {
+                    seriesDetail.removeChild(seriesDetail.firstChild);
+                }
+                var card = createCard(Series_1);
+                seriesDetail.appendChild(card);
+            }
+        });
+    };
+    for (var _i = 0, lista_series_1 = lista_series; _i < lista_series_1.length; _i++) {
+        var Series_1 = lista_series_1[_i];
+        _loop_1(Series_1);
     }
 }
--- TypeScript2/main.ts
+++ TypeScript2/main.ts
@@ -3,30 +3,57 @@ import { Series } from './Series.js';
 
 const tableBody = document.querySelector('#series-table tbody');
 const tableFooter = document.querySelector('#series-table tfoot');
+const seriesDetail = document.querySelector('#series-detail');
 const totalSeries = lista_series.length;
 let totalSeasons = 0;
 
+function createCard(series: Series): HTMLElement {
+  const card = document.createElement('div');
+  card.classList.add('card', 'my-3');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h5');
+  cardTitle.classList.add('card-title');
+  cardTitle.textContent = series.getName();
+  cardBody.appendChild(cardTitle);
+
+  const cardSubtitle = document.createElement('h6');
+  cardSubtitle.classList.add('card-subtitle', 'mb-2', 'text-muted');
+  cardSubtitle.textContent = `Channel: ${series.getChannel()} | Seasons: ${series.getSeasons()}`;
+  cardBody.appendChild(cardSubtitle);
+
+  const cardText = document.createElement('p');
+  cardText.classList.add('card-text');
+  // Replace with actual description if available
+  cardText.textContent = 'No description available.';
+  cardBody.appendChild(cardText);
+
+  card.appendChild(cardBody);
+  return card;
+}
+
 if (tableBody !== null) {
-    for (const Series of lista_series) {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-          <td>${Series.getId()}</td>
-          <td>${Series.getName()}</td>
-          <td>${Series.getChannel()}</td>
-          <td>${Series.getSeasons()}</td>
-        `;
-        tableBody.appendChild(row);
+  for (const Series of lista_series) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${Series.getId()}</td>
+      <td>${Series.getName()}</td>
+      <td>${Series.getChannel()}</td>
+      <td>${Series.getSeasons()}</td>
+    `;
+    tableBody.appendChild(row);
+    totalSeasons += Series.getSeasons();
 
-        totalSeasons += Series.getSeasons();
-    }
-
-    if (tableFooter !== null) {
-      const avgSeasons = totalSeasons / totalSeries;
-      const footerRow = document.createElement('tr');
-      footerRow.innerHTML = `
-        <td colspan="3" style="text-align: right;"><strong>Average Seasons:</strong></td>
-        <td>${avgSeasons.toFixed(2)}</td>
-        `;
-      tableFooter.appendChild(footerRow);
-    }
+    row.addEventListener('click', () => {
+      if (seriesDetail !== null) {
+        while (seriesDetail.firstChild) {
+          seriesDetail.removeChild(seriesDetail.firstChild);
+        }
+        const card = createCard(Series);
+        seriesDetail.appendChild(card);
+      }
+    });
+  }
 }
