54adeb03ef683a062c00c31d0e25ea64fd188a69
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-04-18 00:07:51 -0500

    TS1 Complete

--- TypeScript1/main.ts
+++ TypeScript1/main.ts
@@ -1,17 +1,32 @@
 import { lista_series } from './Data.js';
-import { Series } from './Series';
+import { Series } from './Series.js';
 
 const tableBody = document.querySelector('#series-table tbody');
+const tableFooter = document.querySelector('#series-table tfoot');
+const totalSeries = lista_series.length;
+let totalSeasons = 0;
 
 if (tableBody !== null) {
-    for (const series of lista_series) {
+    for (const Series of lista_series) {
         const row = document.createElement('tr');
         row.innerHTML = `
-          <td>${series.getId()}</td>
-          <td>${series.getName()}</td>
-          <td>${series.getChannel()}</td>
-          <td>${series.getSeasons()}</td>
+          <td>${Series.getId()}</td>
+          <td>${Series.getName()}</td>
+          <td>${Series.getChannel()}</td>
+          <td>${Series.getSeasons()}</td>
         `;
         tableBody.appendChild(row);
+
+        totalSeasons += Series.getSeasons();
+    }
+
+    if (tableFooter !== null) {
+      const avgSeasons = totalSeasons / totalSeries;
+      const footerRow = document.createElement('tr');
+      footerRow.innerHTML = `
+        <td colspan="3" style="text-align: right;"><strong>Average Seasons:</strong></td>
+        <td>${avgSeasons.toFixed(2)}</td>
+        `;
+      tableFooter.appendChild(footerRow);
     }
 }
