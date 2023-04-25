4b0232451d6ee4c6fd10e61b55086ef7d01934bb
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-04-25 11:52:44 -0500

    TalleresAngular

---
+++ TallerAngular/src/app/serie/serie.ts
@@ -0,0 +1,3 @@
+export class Serie {
+  constructor(public id: number, public nombre: string, public channel: string, public seasons: number, public descripcion: string, public webpage: string, public poster: string) {}
+}
