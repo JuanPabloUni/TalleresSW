29f390cd895cebd944534023ac426c3241427568
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-04-26 07:35:52 -0500

    Taller

--- TallerAngular/src/app/app.module.ts
+++ TallerAngular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { SerieModule } from './serie/serie.module';
 
 @NgModule({
@@ -12,6 +13,7 @@ import { SerieModule } from './serie/serie.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SerieModule
   ],
   providers: [],
---
+++ TallerAngular/src/app/serie/serie-list/serie-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from '../serie';
+import { SerieService } from '../serie.service';
+
+@Component({
+  selector: 'app-serie-list',
+  templateUrl: './serie-list.component.html',
+  styleUrls: ['./serie-list.component.css'],
+})
+export class SerieListComponent implements OnInit {
+  series: Array<Serie> = [];
+  seasonsAverage: number = 0;
+
+  constructor(private serieService: SerieService) {}
+
+  getSeries(): void {
+    this.serieService.getSeries().subscribe((series) => {
+      this.series = series;
+    });
+  }
+
+  ngOnInit() {
+    this.getSeries();
+    this.seasonsAverage = this.avg();
+  }
+
+  private avg(): number {
+    const totalSeasons = this.series.reduce((acc, s) => acc + s.seasons, 0);
+    return totalSeasons / this.series.length;
+  }
+}
