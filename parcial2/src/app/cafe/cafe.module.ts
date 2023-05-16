c227cc439ba903faa85b7d0ed01fca957ef767aa
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-05-16 08:22:58 -0500

    Commit

---
+++ parcial2/src/app/cafe/cafe.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CafeListComponent } from './cafe-list/cafe-list.component';
+import { CafeDetailComponent } from './cafe-detail/cafe-detail.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CafeListComponent, CafeDetailComponent],
+  exports: [CafeListComponent]
+})
+export class CafeModule { }
